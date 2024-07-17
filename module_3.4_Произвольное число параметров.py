3dc211136a35743d1cc8a7b6e6716d924e677296
Vadim55-omsk <fvg-income@yandex.ru>
2024-07-17 11:01:30 +0300

    Add files via upload

---
+++ module_3.4_Произвольное число параметров.py
@@ -0,0 +1,16 @@
+
+
+def single_root_words (root_word, *other_word):
+    same_words = []
+    for i in other_word:
+        same_words = 'i', 'gh', 'pop', 'Her', 'Tyu'
+        #if same_words.lower():
+
+
+    return f'{root_word} {same_words}'
+
+
+result1 = single_root_words ("1",'3')
+result2 = single_root_words ('2',"True")
+print(result1)
+print(result2)
